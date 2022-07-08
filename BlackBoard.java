f19a1f203c7c018937878b33b7dda0a8808d894a
jpalle <106265472+jpalle@users.noreply.github.com>
2022-07-07 19:59:36 -0700

    Add files via upload

---
+++ BlackBoard.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Observable;
+
+public class BlackBoard extends Observable {
+    private ArrayList<Point> points;
+    private ArrayList<Line> lines;
+    private static BlackBoard instance;
+    public static BlackBoard getInstance() {
+        if (instance == null)
+            instance = new BlackBoard();
+        return instance;
+    }
+
+    private BlackBoard() {
+        points = new ArrayList<Point>();
+        lines = new ArrayList<Line>();
+    }
+
+    public ArrayList<Point> getPoints() {return points;}
+
+    public ArrayList<Line> getLines () {return lines;}
+
+    public void addPoint (Point p) {
+        points.add(p);
+        setChanged();
+        notifyObservers();
+    }
+
+    public void clearAll() {
+        points.clear();
+        lines.clear();
+        setChanged();
+        notifyObservers();
+    }
+
+    public void setLines (ArrayList<Line> lines) {
+        this.lines = lines;
+    }
+}
